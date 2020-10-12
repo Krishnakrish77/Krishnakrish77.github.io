e2e123da81b21e69ad125df2c165ba3a0e10bc0d
Krishnakrish77 <68425714+Krishnakrish77@users.noreply.github.com>
2020-10-12 16:03:21 +0530

    Update events.js

--- events.js
+++ events.js
@@ -2,13 +2,13 @@ AFRAME.registerComponent('markerhandler', {
 
     init: function() {
         const animatedMarker = document.querySelector("#animated-marker");
-        const aEntity = document.querySelector("#animated-model");
+        const aEntity = document.querySelector("#WebPress");
 
         // every click, we make our model grow in size :)
         animatedMarker.addEventListener('click', function(ev, target){
             const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
             if (aEntity && intersectedElement === aEntity) {
-                window.open("https://www.w3schools.com");
+                window.open("https://voiceofsk.blogspot.com/");
             }
         });
 }});
