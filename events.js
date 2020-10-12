854c74444cd028159fa96ac40f1855298a03419d
Krishnakrish77 <68425714+Krishnakrish77@users.noreply.github.com>
2020-10-12 17:24:35 +0530

    Update events.js

--- events.js
+++ events.js
@@ -26,22 +26,97 @@ AFRAME.registerComponent('markerhandler', {
 AFRAME.registerComponent('clickhandler', {
 
     init: function() {
-        var clicks = false;
+        var lclicks = false;
         const animatedMarker = document.querySelector("#in-marker");
         const aEntity = document.querySelector("#LinkedInPress");
         document.querySelector("#in-marker").addEventListener('markerFound', (evt) => {
-                clicks = true
+                lclicks = true
             })
         document.querySelector("#in-marker").addEventListener('markerLost', (evt) => {
-                clicks = false
+                lclicks = false
             }) 
         
         animatedMarker.addEventListener('click', function(ev, target){
             const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
             if (aEntity && intersectedElement === aEntity) {
-                console.log(clicks);
-                if(clicks) {
+                console.log(lclicks);
+                if(lclicks) {
                     window.open("https://www.linkedin.com/in/sarukrishna-a-119a5681/");
+                }
+            }
+        });
+}});
+
+
+AFRAME.registerComponent('mailhandler', {
+
+    init: function() {
+        var mclicks = false;
+        const animatedMarker = document.querySelector("#mail-marker");
+        const aEntity = document.querySelector("#MailPress");
+        document.querySelector("#mail-marker").addEventListener('markerFound', (evt) => {
+                mclicks = true
+            })
+        document.querySelector("#mail-marker").addEventListener('markerLost', (evt) => {
+                mclicks = false
+            }) 
+        
+        animatedMarker.addEventListener('click', function(ev, target){
+            const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+            if (aEntity && intersectedElement === aEntity) {
+                console.log(mclicks);
+                if(mclicks) {
+                    window.open("mailto:a.sarukrishna@gmail.com");
+                }
+            }
+        });
+}});
+
+
+AFRAME.registerComponent('phhandler', {
+
+    init: function() {
+        var tclicks = false;
+        const animatedMarker = document.querySelector("#ph-marker");
+        const aEntity = document.querySelector("#PhonePress");
+        document.querySelector("#ph-marker").addEventListener('markerFound', (evt) => {
+                tclicks = true
+            })
+        document.querySelector("#ph-marker").addEventListener('markerLost', (evt) => {
+                tclicks = false
+            }) 
+        
+        animatedMarker.addEventListener('click', function(ev, target){
+            const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+            if (aEntity && intersectedElement === aEntity) {
+                console.log(tclicks);
+                if(tclicks) {
+                    window.open("tel:8903587247");
+                }
+            }
+        });
+}});
+
+
+AFRAME.registerComponent('pichandler', {
+
+    init: function() {
+        var pclicks = false;
+        const animatedMarker = document.querySelector("#pic-marker");
+        const aEntity = document.querySelector("#PhotoPress");
+        document.querySelector("#pic-marker").addEventListener('markerFound', (evt) => {
+                pclicks = true
+            })
+        document.querySelector("#pic-marker").addEventListener('markerLost', (evt) => {
+                pclicks = false
+            }) 
+        
+        animatedMarker.addEventListener('click', function(ev, target){
+            const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+            if (aEntity && intersectedElement === aEntity) {
+                console.log(pclicks);
+                if(pclicks) {
+                    window.open("https://drive.google.com/file/d/1I6JGCXRRGq6RlCwDjdNPDFHJIYOlK_L6/view?usp=sharing");
                 }
             }
         });
