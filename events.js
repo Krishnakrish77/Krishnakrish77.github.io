f8abb5646eea7bb73f8c546ea8d140fc4bedf795
Krishnakrish77 <68425714+Krishnakrish77@users.noreply.github.com>
2020-10-12 17:01:27 +0530

    Update events.js

--- events.js
+++ events.js
@@ -21,3 +21,28 @@ AFRAME.registerComponent('markerhandler', {
             }
         });
 }});
+
+
+AFRAME.registerComponent('clickhandler', {
+
+    init: function() {
+        var clicks = false;
+        const animatedMarker = document.querySelector("#in-marker");
+        const aEntity = document.querySelector("#LinkedInPress");
+        document.querySelector("#in-marker").addEventListener('markerFound', (evt) => {
+                clicks = true
+            })
+        document.querySelector("#in-marker").addEventListener('markerLost', (evt) => {
+                clicks = false
+            }) 
+        
+        animatedMarker.addEventListener('click', function(ev, target){
+            const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+            if (aEntity && intersectedElement === aEntity) {
+                console.log(clicks);
+                if(clicks) {
+                    window.open("https://www.linkedin.com/in/sarukrishna-a-119a5681/");
+                }
+            }
+        });
+}});
