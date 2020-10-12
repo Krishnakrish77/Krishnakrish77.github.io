6f38b36bf92078b8b6a1fadb7d646fb3df19bb93
Krishnakrish77 <68425714+Krishnakrish77@users.noreply.github.com>
2020-10-12 16:18:58 +0530

    Update events.js

--- events.js
+++ events.js
@@ -1,14 +1,25 @@
+var clicks = false;
+function flag(){
+    document.querySelector("#scene").addEventListener('markerFound', (evt) => {
+                clicks = true
+            })
+    document.querySelector("#scene").addEventListener('markerLost', (evt) => {
+                clicks = false
+            }) 
+};
+
 AFRAME.registerComponent('markerhandler', {
 
     init: function() {
         const animatedMarker = document.querySelector("#animated-marker");
         const aEntity = document.querySelector("#WebPress");
-
+        var fl=flag();
         // every click, we make our model grow in size :)
         animatedMarker.addEventListener('click', function(ev, target){
             const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
             if (aEntity && intersectedElement === aEntity) {
-                window.open("https://voiceofsk.blogspot.com/");
+                if(fl)
+                    window.open("https://voiceofsk.blogspot.com/");
             }
         });
 }});
